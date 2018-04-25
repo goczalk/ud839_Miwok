3972e04e5b15ce5788a154baa436e47dc4264c99
Klaudia Goczal <klaudia@lodz.pl>
2018-04-25 23:45:54 +0200

    Added for loop to create TextViews

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,12 +17,37 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+    private ArrayList<String> words = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.root_view);
+
+        for(int i = 0; i < words.size(); i++) {
+            TextView textView = new TextView(this);
+            textView.setText(words.get(i));
+            rootView.addView(textView);
+        }
     }
 }
