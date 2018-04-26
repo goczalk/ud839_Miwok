b9cdaa70db25b925263ba88a5aa33184f891508a
Klaudia Goczal <klaudia@lodz.pl>
2018-04-27 00:20:04 +0200

    Changed to ArrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,7 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -42,12 +44,11 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.root_view);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(
+                                            this, android.R.layout.simple_list_item_1, words);
 
-        for(int i = 0; i < words.size(); i++) {
-            TextView textView = new TextView(this);
-            textView.setText(words.get(i));
-            rootView.addView(textView);
-        }
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
