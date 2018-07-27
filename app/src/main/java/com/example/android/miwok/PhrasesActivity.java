174ec812c9ef07225fc31c701cb5fb0da4c08dc7
Klaudia Goczal <klaudia@lodz.pl>
2018-07-27 14:26:15 +0200

    Custom solution to Audio FOcus in PhrasesActivity

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +29,7 @@ import java.util.ArrayList;
 public class PhrasesActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +58,16 @@ public class PhrasesActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Word word = words.get(i);
                 releaseMediaPlayer();
-                mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getMp3ResourceId());
-                mMediaPlayer.start();
 
+                mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getMp3ResourceId());
+
+                mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                int audioManagerResult = mAudioManager.requestAudioFocus(
+                        mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if(audioManagerResult == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer.start();
+                }
                 mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
@@ -91,8 +101,44 @@ public class PhrasesActivity extends AppCompatActivity {
         public void onCompletion(MediaPlayer mediaPlayer) {
             // Now that the sound file has finished playing, release the media player resources.
             releaseMediaPlayer();
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
         }
     };
 
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                // Pause playback because your Audio Focus was
+                // temporarily stolen, but will be back soon.
+                // i.e. for a phone call
+                mMediaPlayer.pause();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // Stop playback, because you lost the Audio Focus.
+                // i.e. the user started some other playback app
+                // Remember to unregister your controls/buttons here.
+                // And release the kra — Audio Focus!
+                // You’re done.
+                mMediaPlayer.stop();
+                mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+            } else if (focusChange ==
+                    AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // Lower the volume, because something else is also
+                // playing audio over you.
+                // i.e. for notifications or navigation directions
+                // Depending on your audio playback, you may prefer to
+                // pause playback here instead. You do you.
+                mMediaPlayer.pause();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // Resume playback, because you hold the Audio Focus
+                // again!
+                // i.e. the phone call ended or the nav directions
+                // are finished
+                // If you implement ducking and lower the volume, be
+                // sure to return it to normal here, as well.
+                mMediaPlayer.start();
+            }
+        }
+    };
 }
 
